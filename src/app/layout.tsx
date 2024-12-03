23b86e68103700ed402549e3daef20dfd27b14ad
Munkhjin3 <muujii@motforex.com>
2024-12-03 15:05:25 +0800

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Navbar } from "@/components";
+import { Footer, Navbar } from "@/components";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +33,8 @@ export default function RootLayout({
         <Navbar/>
         
         {children}
+        <Footer/>
+
       </body>
     </html>
   );
--- src/components/common/container.tsx
+++ src/components/common/container.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 export const Container = ({ children, containerClassName, className }: ContainerPropsType) => {
   return (
     <div className={cn('mt-5 flex w-full flex-col items-center', containerClassName)}>
-      <div className={cn('flex w-full max-w-[1824px] flex-col items-center max-lg:px-2 lg:px-8', className)}>
+      <div className={cn('flex w-full max-w-[1440px] flex-col items-center max-lg:px-2 lg:px-8', className)}>
         {children}
       </div>
     </div>
